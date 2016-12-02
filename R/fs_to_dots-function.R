94f36a04b6cdcd25f3ad779e2d112801d1899324
Darrell Jonathan Penta <d.penta@neu.edu>
2016-12-01 22:21:47 -0500

    cleaning function description

--- R/fs_to_dots-function.R
+++ R/fs_to_dots-function.R
@@ -1,18 +1,16 @@
 #' Create quoted input for NSE \code{.dots}
 #'
 
-#' \code{flist_to_dots} takes a list of formulas or a character vector of such a list and \"quotes\" it for passing to the \code{.dots} argument of the NSE versions of dplyr \code{\link[dplyr]{nse}} functions like \code{\link[dplyr]{select_}}. It can be useful in cases where you've got a list of conditions in a separate dataframe, etc.
+#' \code{fs_to_dots} takes a list of formulas or a character vector of such a list and "quotes" it for passing to the \code{.dots} argument of the NSE versions of dplyr \code{\link[dplyr]{nse}} functions like \code{\link[dplyr]{select_}}. Can be useful if the conditions for tidying and wrangling a dataset are specified in a list, for example.
 #'
 #' @param formlist Either a list of comma-separated RHS formulas, as \code{list(~f1, ~f2)}, or a character vector of such a list: \code{"list(~col1,col2)"}.
 #' @return A quoted expression that can be interpred as a \code{dots} argument.
 #' @examples
-#'  ## With a comma-separated list of RHS formulas
+#'
+#' # With a comma-separated list of RHS formulas
 #'
 #' filter_list <-
-#'   list(~Sepal.Length < 5.0, ~Petal.Length > 1.3)
-#'
-#' filter_list <-
-#'   flist_to_dots(filter_list)
+#'   flist_to_dots(list(~Sepal.Length < 5.0, ~Petal.Length > 1.3))
 #'
 #' head(
 #' iris %>%
@@ -24,7 +22,9 @@
 #' df <-
 #'   dplyr::tibble(
 #'     col1 = 1:2,
-#'     subset_form = c("list(~Sepal.Length,~Petal.Length)", "list(~Sepal.Length)"))
+#'     col2 = c("A","Z")
+#'     dot_forms = c("list(~Sepal.Length,~Petal.Length)",
+#'     "list(~Sepal.Length)"))
 #'
 #' select_form <-
 #'   flist_to_dots(df$filter_form[2])
@@ -35,7 +35,7 @@
 #'
 #' @export
 
-flist_to_dots <-
+fs_to_dots <-
   function(formlist) {
   if(!(is.character(formlist) | is.list(formlist))) { stop("Formlist should be either a list of comma-separated RHS formulas, as 'list(~f1, ~f2)', or a character vector of such a list: \"list(~col1,col2)\"")
   } else if(is.list(formlist)) {
