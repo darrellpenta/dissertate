1824916ccf908eb2126b028d61c1a9bd4da3d930
Darrell Jonathan Penta <d.penta@neu.edu>
2017-02-02 10:36:08 -0500

    updated cc function

--- R/cc-function.R
+++ R/cc-function.R
@@ -4,7 +4,8 @@
 #' \code{cc} is a wrapper around \code{\link[stringr]{str_split}} that takes a single quoted string whose elements are to be split at whitespace.
 #'
 #' @param string_to_split A single quoted string.
-#' @return A simplified character matrix
+#' @param type One of \code{c} (default) to return a character vector; \code{i} to return an integer vector; \code{n} to return a numeric vector.
+#' @return A character, integer, or numeric vector.
 #' @examples
 #'
 #' # compare: c("A","B","C") vs. cc("A B C")
@@ -19,7 +20,20 @@
 
 
 cc <-
-  function(string_to_split){
+  function(string_to_split, type = "c") {
     string_to_split <-
-      stringr::str_split(string_to_split,pattern = "[\\s]{1,3}",simplify = TRUE)
+      stringr::str_split(string_to_split, pattern = "[\\s]{1,3}", simplify = FALSE)
+    string_to_split <-
+      unlist(string_to_split)
+    if (type == "c") {
+      string_to_split <-
+        as.character(string_to_split)
+    } else if (type == "i") {
+      string_to_split <-
+        as.integer(string_to_split)
+    } else if (type == "n") {
+      string_to_split <-
+        as.numeric(string_to_split)
+    }
   }
+
