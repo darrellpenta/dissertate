88a5b87e7ccc8b6786ad01fc9fd2bd806350d213
Darrell Jonathan Penta <d.penta@neu.edu>
2017-03-08 11:19:20 -0500

    added root_dir function

---
+++ R/root_dir-function.R
@@ -0,0 +1,20 @@
+#' Shortcut for specifying paths relative to root.
+#'
+
+#' \code{root_dir} is a wrapper around \code{getwd} that appends the path-part to the location specified by the user-supplied argument.
+#'
+#' @param pathpart A string to the path}.
+#' @return A character vector with the path to the root directory appended to the path-part specified.
+#' @examples
+#'
+#' # Supply path-part without preceding forward slash
+#'
+#' my_path <-
+#'  root_dir("myfolder/test.txt")
+#'
+#'  my_path
+#' @export
+
+root_dir <-
+  function(pathpart) {paste0(as.character(getwd()),"/",pathpart, collapse = "")}
+
