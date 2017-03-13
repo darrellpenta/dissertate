acce0062e9602f2714aff3cd8551c27ac09f2a99
Darrell Jonathan Penta <d.penta@neu.edu>
2017-03-12 23:14:10 -0400

    update to greater_than function

--- R/get_range_grt_than-function.R
+++ R/get_range_grt_than-function.R
@@ -4,12 +4,16 @@
 #' May be useful when non-singificant values are going to be displayed or reported.
 #'
 #' @param p A number for which a p-value rage should be returned.
+#' @param print_marg Should marginally significant values be printed? Set to FALSE
 #' @return The significance range: e.g., "<.001"
 #' @export
 
-get_range_grt_than = function(p) {
+get_range_grt_than = function(p, print_marg = FALSE) {
   range = findInterval(p, c(0, 0.001, 0.01, 0.05, 0.1))
-  codes = c("<.001" , "<.01", "<.05", "<.10", ">.10")
+  codes = c("<.001" ,
+            "<.01",
+            "<.05",
+            ifelse(isTRUE(print_marg),paste0("=",round(p,3)),"<.10"), ">.10")
   codes[range]
 }
 options(scipen = 1)
